f72835c986ea65432c77b82441f19ee294e8c662
Harrison Billings <harry-mac@Harrisons-Laptop.local>
2025-04-07 20:59:17 -0700

    second

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"embed"
+	"os"
+	"strings"
 
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/cmd"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	// enable once you have at least one migration
+	_ "frontend-friend/migrations"
 )
 
 //go:embed all:frontend/dist
@@ -15,11 +23,28 @@ func main() {
 	// Create an instance of the app structure
 	app := NewApp()
 
+	// Go Routine to run Pocketbase
+	go func() {
+		pocketBaseApp := pocketbase.New()
+
+		// loosely check if it was executed using "go run"
+		isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+		migratecmd.MustRegister(pocketBaseApp, pocketBaseApp.RootCmd, migratecmd.Config{
+			// enable auto creation of migration files when making collection changes in the Dashboard
+			// (the isGoRun check is to enable it only during development)
+			Automigrate: isGoRun,
+		})
+
+		pocketBaseApp.Bootstrap()
+		serveCmd := cmd.NewServeCommand(pocketBaseApp, true)
+		serveCmd.Execute()
+	}()
+
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "frontend-friend",
-		Width:  1024,
-		Height: 768,
+		Title:            "frontend-friend",
+		WindowStartState: options.WindowStartState(3),
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
